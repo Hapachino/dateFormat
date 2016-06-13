9209b32238e6397ca4e01540a60d3a9a47005ce7
cafeKaze <erickwbrownfield@gmail.com>
2016-06-13 13:45:10 -0700

    Update dateFormat.py

--- dateFormat.py
+++ dateFormat.py
@@ -1,23 +1,60 @@
 #! python3
-# dateFormat: Finds all file names in current working directory, revises American-style dates (MM-DD-YYYY) to European-style (DD-MM-YYYY)
+# dateFormat: Finds all file names in cwd, revises American-style dates (MM-DD-YYYY) to European-style (DD-MM-YYYY)
 
 import os, re, shutil
 
-# Create a regex that can identify the text pattern of American-style dates.
+# regex that can identify the text pattern of American-style dates.
 
-dateRegex = re.compile(r'''(\d{2})	# Month
-			   -
-			   (\d{2})	# Day
-			   -
-			   (\d{4})'''	# Year
-			   , re.VERBOSE)
+dateRegex = re.compile(r'''
+			^(.*?)?		# Text Before
+			((0|1)\d)	# Month
+			-		
+			((0|1|2|3)\d)	# Day
+			-
+			((19|20)\d\d)	# Year
+			(.*?)?$		# Text After
+			''', re.VERBOSE)
 
-# Loop over each filename in current working directory, using the regex to check whether it has a American date 
-# and rename with Shutil.move():
+''' 
+Numeric representation of the mo.groups:
 
-for filename in os.listdir(os.getcwd()):
-	if dateRegex.search(filename) != None:
-		shutil.move(filename, dateRegex.sub('\2-\1-\3', filename))
-		
+			^(1)?		# Text Before
+			(2(3))		# Month
+			-		
+			(4(5))		# Day
+			-
+			(6(7))		# Year
+			(8)?$		# Text After
+'''
+
+# Loop over each filename in current working directory, using the regex to check whether it has an American date 
+
+for filename in os.listdir('.'):
+	mo = dateRegex.search(filename)
+	if mo == None
+		continue
+
+# if match found, create variales of the mo.groups:
+
+	textBefore = mo.group(1)
+	month = mo.group(2)
+	day = mo.group(4)
+	year = mo.group(6)
+	textAfter = mo.group(8)
+	
+# create European style filename using mo.group variables
+	
+	europeanFile = textBefore + month + '-' + day + '-' + year + textAfter
+
+# find the American and Euroepan absolute filepaths and rename with shutil.move(): comment out and use print first!
+
+	cwd = os.path.abspath('.')
+	europeanPath = os.path.join(cwd, europeanFile)
+	americanPath = os.path.join(cdw, filename)
+	# shutil.move(americanPath, europeanPath) 	# uncomment after testing
+	print('Renaming "%s" to "%s"...' % (americanPath, europeanPath))
+	
+
+
 
 
